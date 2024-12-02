91602d26f492de84d7f7db53e15214be959f242b
Augustus1417 <jedxyril.cruz@gmail>
2024-12-02 12:50:43 +0800

    hahah

--- modules/print_all_students.py
+++ modules/print_all_students.py
@@ -3,7 +3,5 @@ class Print_All_Students:
         self.student_data = student
 
     def print_all_students(self):
-        print("\n======= All Student Information =======")
         for student in self.student_data.allstudents:
             return (student)
-        print("\n=========== Nothing Follows ===========\n")
